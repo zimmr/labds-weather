843c93cf44a04e3a1126e9fc78ec3df0f181cbdf
zimmr <joaozimmr@gmail.com>
2026-05-07 00:50:37 -0300

    Cria classe User

---
+++ app/backend/clima-api/src/model/User.java
@@ -0,0 +1,29 @@
+package model;
+
+public class User {
+    private String Id;
+    private String Name;
+    private String Email;
+    private String Password;
+    private boolean Celsius;
+    // TODO: histórico
+    // TODO: favoritos
+    // TODO: gets e sets
+    // TODO: outros métodos
+
+    public User(String id, String name, String email, String password, boolean celsius) {
+        Id = id;
+        Name = name;
+        Email = email;
+        Password = password;
+        Celsius = celsius;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+}
