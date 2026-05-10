a2cb3df35344faa3044dd42178415ac76b12a4a5
zimmr <joaozimmr@gmail.com>
2026-05-09 22:07:21 -0300

    :memo: todo list etc

--- app/backend/clima-api/src/services/BaseOpenWeatherApiService.java
+++ app/backend/clima-api/src/services/BaseOpenWeatherApiService.java
@@ -3,9 +3,6 @@ package services;
 import utils.Config;
 
 public class BaseOpenWeatherApiService extends BaseService {
-
-    // "https://api.openweathermap.org/data/2.5/weather?lat=-29.9202596&lon=-51.18361&appid={{apiKey}}&units=metric"
     protected String baseUrl = Config.get("openweather.baseUrl");
     protected String apiKey = Config.get("openweather.apiKey");
-
 }
