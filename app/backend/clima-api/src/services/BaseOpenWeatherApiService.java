d7be6448025cad86e5e2177fb022e2e4f4bed57e
zimmr <joaozimmr@gmail.com>
2026-05-11 19:53:58 -0300

    :art: Simplifica estrutura de services

--- app/backend/clima-api/src/services/BaseOpenWeatherApiService.java
+++ app/backend/clima-api/src/services/BaseOpenWeatherApiService.java
@@ -1,8 +1,22 @@
 package services;
 
-import utils.Config;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 
-public class BaseOpenWeatherApiService extends BaseApiService {
+import utils.Config;
+import utils.JsonUtils;
+
+public class BaseOpenWeatherApiService extends BaseService {
     protected String baseUrl = Config.get("openweather.baseUrl");
     protected String apiKey = Config.get("openweather.apiKey");
+
+    protected String sendRequest(String path) throws MalformedURLException, IOException {
+        URL url = URI.create(path).toURL();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        
+        return JsonUtils.getJsonFromStream(conn.getInputStream());
+    }
 }
