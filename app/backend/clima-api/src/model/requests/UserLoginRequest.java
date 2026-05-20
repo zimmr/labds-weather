5fcfec9fe6108c27ff27c79e01adf50853f1a231
zimmr <joaozimmr@gmail.com>
2026-05-20 20:17:37 -0300

    :sparkles: Endpoint login

---
+++ app/backend/clima-api/src/model/requests/UserLoginRequest.java
@@ -0,0 +1,6 @@
+package model.requests;
+
+public class UserLoginRequest {
+    public String email;
+    public String password;
+}
--- app/backend/clima-api/src/services/IUserService.java
+++ app/backend/clima-api/src/services/IUserService.java
@@ -5,9 +5,11 @@ import java.net.MalformedURLException;
 
 import model.entities.User;
 import model.requests.GetUserRequest;
+import model.requests.UserLoginRequest;
 import model.requests.UserRequest;
 
 public interface IUserService {
         public User get(GetUserRequest request) throws MalformedURLException, IOException, Exception;
         public User save(UserRequest request) throws MalformedURLException, IOException, Exception;
+        public User login(UserLoginRequest request) throws MalformedURLException, IOException, Exception;
 }
