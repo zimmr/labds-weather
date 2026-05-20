72b1b8529b3528d0248eb9fd6c028a8389e053f4
zimmr <joaozimmr@gmail.com>
2026-05-20 20:17:56 -0300

    :adhesive_bandage: fix HashUtils

--- app/backend/clima-api/src/utils/HashUtils.java
+++ app/backend/clima-api/src/utils/HashUtils.java
@@ -5,7 +5,7 @@ import org.mindrot.jbcrypt.BCrypt;
 public class HashUtils {
 
     public static String create(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+        return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
     public static boolean compare(String password, String hash) {
