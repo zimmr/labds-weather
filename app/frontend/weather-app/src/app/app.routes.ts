4d57c39f401ade17792681931ff8b3b5dcf76bf7
AlmeidaMatheus <matheusdalmeida867@gmail.com>
2026-05-20 21:37:02 -0300

    Adicionando componente de login e cadastro

--- app/frontend/weather-app/src/app/app.routes.ts
+++ app/frontend/weather-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/components/login/login';
+import { Cadastro } from './cadastro/components/cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Login
+    },
+    {
+        path: 'register',
+        component: Cadastro
+    },
+];
---
+++ app/frontend/weather-app/src/app/cadastro/components/cadastro/cadastro.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [RouterLink],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.css',
+})
+export class Cadastro {}
