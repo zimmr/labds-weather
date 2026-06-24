d23e2cba1a7be6a0fe39e7c6f7bc50a4d53c2ad9
zimmr <joaozimmr@gmail.com>
2026-06-23 23:09:10 -0300

    Implemetação do login no front

--- app/frontend/weather-app/src/app/login/components/login/login.ts
+++ app/frontend/weather-app/src/app/login/components/login/login.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+  erro = '';
+  carregando = false;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  login() {
+    this.erro = '';
+    this.carregando = true;
+
+    this.userService.login(this.email, this.password).subscribe({
+      next: (res) => {
+        console.log(res);
+
+        localStorage.setItem('login', this.email);
+        localStorage.setItem('password', this.password);
+
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.erro = err.status === 401
+          ? 'Email ou senha incorretos.'
+          : 'Erro ao conectar com o servidor.';
+        this.carregando = false;
+      },
+    });
+  }
+}
