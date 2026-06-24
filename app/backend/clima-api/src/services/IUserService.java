27f2abffe9d309281b2d09fee3fd7a2f0aab7756
zimmr <joaozimmr@gmail.com>
2026-06-23 23:13:26 -0300

    Ajuste no retorno da rota de login

--- app/backend/clima-api/src/services/IUserService.java
+++ app/backend/clima-api/src/services/IUserService.java
@@ -12,10 +12,11 @@ import model.requests.GetUserRequest;
 import model.requests.RequestHeaders;
 import model.requests.UserLoginRequest;
 import model.requests.UserRequest;
+import model.responses.UserResponse;
 
 public interface IUserService {
         public User get(GetUserRequest request) throws MalformedURLException, IOException, Exception;
         public User save(UserRequest request) throws MalformedURLException, IOException, Exception;
-        public User login(UserLoginRequest request) throws MalformedURLException, IOException, Exception;
+        public UserResponse login(UserLoginRequest request) throws MalformedURLException, IOException, Exception;
         public AuthenticationResultDto authenticate(User user, RequestHeaders headers, boolean isObligatory) throws IllegalArgumentException, NoSuchElementException, AuthenticationException;
 }
